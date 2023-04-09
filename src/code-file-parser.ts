1b12e3c2e6098bfab598c3d9317f06cdfa4a0cdd
Josh J - Desktop <jjosephttg@gmail.com>
2023-04-09 19:43:17 -0400

    Add type for clarity

--- src/code-file-parser.ts
+++ src/code-file-parser.ts
@@ -1,8 +1,13 @@
 import { readFile } from 'fs/promises';
 import path from 'path';
 
+export type CodeFile = {
+  filePath: string;
+  rawContent: string;
+};
+
 export class CodeFileParser {
-  public async parseFile(filePath: string) {
+  public async parseFile(filePath: string): Promise<CodeFile> {
     const rawContent = await readFile(filePath, 'utf-8');
     return {
       filePath: path.resolve(filePath),
